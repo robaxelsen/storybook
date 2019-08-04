a489be986a18f64d4dc49fc1aa9335bcce99dfdf
Gaëtan Maisse <gmaisse@solocal.com>
2019-08-04 17:24:32 +0200

    refactor: move angular loader in addon-storyshots framework part to TS

--- addons/storyshots/storyshots-core/src/frameworks/Loader.ts
+++ addons/storyshots/storyshots-core/src/frameworks/Loader.ts
@@ -1,4 +1,15 @@
+type SupportedFramework = 'angular';
+
+export type RenderTree = (story: any) => any;
+
 export interface Loader {
-  load: (options: any) => any;
+  load: (
+    options: any
+  ) => {
+    framework: SupportedFramework;
+    renderTree: RenderTree;
+    renderShallowTree: any;
+    storybook: any;
+  };
   test: (options: any) => boolean;
 }
